c31517e7dd57f8aac45d49f502ca7dc6ed19963c
Evi Tzvi <71942613+tz17012016@users.noreply.github.com>
2021-07-14 15:28:20 +0300

    Update mongoConnect.js

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -1,14 +1,4 @@
-/*const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/hipo5', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("mongo connected");
-});
-
-module.exports = db;
-*/
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
